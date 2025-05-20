ed4d61209f8ca61b1fa8e7efad3a9e9777362d23
wrafalit <wrafalit@gmail.com>
2025-05-20 14:23:51 +0200

    add js

--- src/main/java/com/appium/example/util/driver/MobileDriverService.java
+++ src/main/java/com/appium/example/util/driver/MobileDriverService.java
@@ -4,16 +4,23 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
+import java.io.File;
+
 import static com.appium.example.constant.DriverConstants.APPIUM_SERVER_IP;
 import static com.appium.example.constant.DriverConstants.APPIUM_SERVER_PORT;
 
 public interface MobileDriverService {
+
     default AppiumDriverLocalService startAppiumService() {
+        File appiumMainJs = new File("C:/Users/omenr/AppData/Roaming/npm/node_modules/appium/build/lib/main.js");
+
         AppiumDriverLocalService appiumService = AppiumDriverLocalService.buildService(
                 new AppiumServiceBuilder()
+                        .withAppiumJS(appiumMainJs)
                         .withIPAddress(APPIUM_SERVER_IP)
                         .usingPort(APPIUM_SERVER_PORT)
         );
+
         appiumService.start();
         return appiumService;
     }
